aaf3806fbda8daff518efa825f1b7258e95b1860
Raphael Neumann <raphael@rneumann.me>
2020-01-07 16:31:30 -0300

    Include dependency on setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     version = '0.0.1',
     packages = ['grpcLoadTest'],
     scripts=['./scripts/grpcLoadTest'],
+    install_requires = [
+        'grpcio',
+        'grpcio-tools',
+        'argparse',
+    ],
     entry_points = {
         'console_scripts': [
             'pycli = grpcLoadTest.__main__:main'
