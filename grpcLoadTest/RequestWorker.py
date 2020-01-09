37f761a9ceae06744f479f205a375f9f4a0b8d1e
Raphael Neumann <raphael@rneumann.me>
2020-01-09 13:44:09 -0300

    update grpcLoadTest/RequestWorker.py

--- grpcLoadTest/RequestWorker.py
+++ grpcLoadTest/RequestWorker.py
@@ -1,15 +1,16 @@
 from threading import Thread
 
 class RequestWorker(Thread):
-    def __init__(self, requestObj, requests, bidirectional=False):
+    def __init__(self, requestObj, requests, report, bidirectional=False):
         Thread.__init__(self)
         self.request = requestObj
         self.bidirectional = bidirectional
         self.requests=requests
+        self.report=report
 
     def run(self):
         for x in range(self.requests):
             if self.bidirectional:
-                print(self.request.streamRequest())
+                self.report.append(self.request.streamRequest())
             else:
-                print(self.request.basicRequest())
+                self.report.append(self.request.basicRequest())
